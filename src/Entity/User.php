8df6ed637d3bb062971d0fcea08ac3723e69b527
Chaima-Cherif391 <Cherif.Chaima@esprit.tn>
2025-04-16 13:07:54 +0100

    relation user tache

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -79,52 +79,160 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     // === GETTERS & SETTERS ===
 
-    public function getId(): ?int { return $this->id_employe; }
+    public function getId(): ?int
+    {
+        return $this->id_employe;
+    }
 
-    public function getFirstname(): string { return $this->firstname; }
-    public function setFirstname(string $firstname): self { $this->firstname = $firstname; return $this; }
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
 
-    public function getLastname(): string { return $this->lastname; }
-    public function setLastname(string $lastname): self { $this->lastname = $lastname; return $this; }
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
 
-    public function getEmail(): string { return $this->email; }
-    public function setEmail(string $email): self { $this->email = $email; return $this; }
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
 
-    public function getPassword(): string { return $this->password; }
-    public function setPassword(string $password): self { $this->password = $password; return $this; }
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
 
-    public function getProfilePhoto(): ?string { return $this->profilePhoto; }
-    public function setProfilePhoto(?string $profilePhoto): self { $this->profilePhoto = $profilePhoto; return $this; }
+    public function getProfilePhoto(): ?string
+    {
+        return $this->profilePhoto;
+    }
+    public function setProfilePhoto(?string $profilePhoto): self
+    {
+        $this->profilePhoto = $profilePhoto;
+        return $this;
+    }
 
-    public function getBirthdayDate(): ?\DateTimeInterface { return $this->birthdayDate; }
-    public function setBirthdayDate(?\DateTimeInterface $birthdayDate): self { $this->birthdayDate = $birthdayDate; return $this; }
+    public function getBirthdayDate(): ?\DateTimeInterface
+    {
+        return $this->birthdayDate;
+    }
+    public function setBirthdayDate(?\DateTimeInterface $birthdayDate): self
+    {
+        $this->birthdayDate = $birthdayDate;
+        return $this;
+    }
 
-    public function getJoiningDate(): ?\DateTimeInterface { return $this->joiningDate; }
-    public function setJoiningDate(?\DateTimeInterface $joiningDate): self { $this->joiningDate = $joiningDate; return $this; }
+    public function getJoiningDate(): ?\DateTimeInterface
+    {
+        return $this->joiningDate;
+    }
+    public function setJoiningDate(?\DateTimeInterface $joiningDate): self
+    {
+        $this->joiningDate = $joiningDate;
+        return $this;
+    }
 
-    public function getRole(): string { return $this->role; }
-    public function setRole(string $role): self { $this->role = $role; return $this; }
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+        return $this;
+    }
 
-    public function getTtRestants(): ?int { return $this->tt_restants; }
-    public function setTtRestants(?int $tt_restants): self { $this->tt_restants = $tt_restants; return $this; }
+    public function getTtRestants(): ?int
+    {
+        return $this->tt_restants;
+    }
+    public function setTtRestants(?int $tt_restants): self
+    {
+        $this->tt_restants = $tt_restants;
+        return $this;
+    }
 
-    public function getCongeRestant(): ?int { return $this->conge_restant; }
-    public function setCongeRestant(?int $conge_restant): self { $this->conge_restant = $conge_restant; return $this; }
+    public function getCongeRestant(): ?int
+    {
+        return $this->conge_restant;
+    }
+    public function setCongeRestant(?int $conge_restant): self
+    {
+        $this->conge_restant = $conge_restant;
+        return $this;
+    }
 
-    public function getUploadedCv() { return $this->uploaded_cv; }
-    public function setUploadedCv($uploaded_cv): self { $this->uploaded_cv = $uploaded_cv; return $this; }
+    public function getUploadedCv()
+    {
+        return $this->uploaded_cv;
+    }
+    public function setUploadedCv($uploaded_cv): self
+    {
+        $this->uploaded_cv = $uploaded_cv;
+        return $this;
+    }
 
-    public function getNumTel(): ?int { return $this->num_tel; }
-    public function setNumTel(?int $num_tel): self { $this->num_tel = $num_tel; return $this; }
+    public function getNumTel(): ?int
+    {
+        return $this->num_tel;
+    }
+    public function setNumTel(?int $num_tel): self
+    {
+        $this->num_tel = $num_tel;
+        return $this;
+    }
 
-    public function getGender(): ?string { return $this->gender; }
-    public function setGender(?string $gender): self { $this->gender = $gender; return $this; }
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+    public function setGender(?string $gender): self
+    {
+        $this->gender = $gender;
+        return $this;
+    }
 
-    public function isBirthdateEdited(): bool { return $this->birthdate_edited; }
-    public function setBirthdateEdited(bool $birthdate_edited): self { $this->birthdate_edited = $birthdate_edited; return $this; }
+    public function isBirthdateEdited(): bool
+    {
+        return $this->birthdate_edited;
+    }
+    public function setBirthdateEdited(bool $birthdate_edited): self
+    {
+        $this->birthdate_edited = $birthdate_edited;
+        return $this;
+    }
 
-    public function isFirstLogin(): bool { return $this->first_login; }
-    public function setFirstLogin(bool $first_login): self { $this->first_login = $first_login; return $this; }
+    public function isFirstLogin(): bool
+    {
+        return $this->first_login;
+    }
+    public function setFirstLogin(bool $first_login): self
+    {
+        $this->first_login = $first_login;
+        return $this;
+    }
 
     public function getTaches(): Collection
     {
@@ -155,17 +263,32 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     // === USER INTERFACE METHODS ===
 
-    public function getUserIdentifier(): string { return $this->email; }
-    public function getRoles(): array { return [$this->role]; }
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+    public function getRoles(): array
+    {
+        return [$this->role];
+    }
     public function eraseCredentials(): void {}
 
     // === UTILS ===
 
-    public function __toString(): string {
-        return sprintf("Employee{id=%d, firstname='%s', lastname='%s', email='%s', role='%s'}",
-            $this->id_employe, $this->firstname, $this->lastname, $this->email, $this->role
+    public function __toString(): string
+    {
+        return sprintf(
+            "Employee{id=%d, firstname='%s', lastname='%s', email='%s', role='%s'}",
+            $this->id_employe,
+            $this->firstname,
+            $this->lastname,
+            $this->email,
+            $this->role
         );
     }
 
-    public function equals(User $employee): bool { return $this->id_employe === $employee->getId(); }
+    public function equals(User $employee): bool
+    {
+        return $this->id_employe === $employee->getId();
+    }
 }
