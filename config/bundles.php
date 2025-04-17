085b0766c44fc5deff98e044d60e23a1cb0a5296
Farahchebane22 <chebanefarah@gmail.com>
2025-04-17 10:08:19 +0100

    merge farah

--- config/bundles.php
+++ config/bundles.php
@@ -9,7 +9,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 
-    //Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
    //Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 
